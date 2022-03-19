05495ee1d0e078ded23713b7f277a0f9838ec940
JoakimLindbom <Joakim.Lindbom@gmail.com>
2022-03-19 22:06:42 +0100

    Added globals

---
+++ apps/globals.py
@@ -0,0 +1,22 @@
+#################
+#
+# Global constants and Ids
+#
+
+import hassapi as hass
+
+FADE = "fade"
+ON = "on"
+OFF = "off"
+
+#Family states
+test123 = "testabc"
+WORKING = "Working"
+NON_WORKING = "Non_working"
+VACATION = "Vacation (away)"
+family_state_sensor = "input_select.family_mode"  # TODO: Get from apps.yaml in __init__ ??
+
+class globals (hass.Hass):
+    test321 = "test321"
+    def initialize(self):
+        pass
